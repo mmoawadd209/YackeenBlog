1f3b5041c862a4d559c3205d653e895bbf122db1
mmoawadd209 <mmoawadd209@gmail.com>
2019-10-09 23:43:23 +0200

    Add project files.

---
+++ YackeenBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YackeenBlog.MvcApplication" Language="C#" %>
---
+++ YackeenBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YackeenBlog.Startup))]
+namespace YackeenBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
